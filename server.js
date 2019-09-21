385122462ce902ddbd44b2899bee21ae11117afa
NguyenPhucNam <nnam1472@gmail.com>
2019-09-21 13:12:10 +0700

    balancing && redis

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const config = require('./config/config');
 const server = require("http").Server(app);
 
 const port = process.env.PORT || config.Port;
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log("Start server: " + port);
 });
