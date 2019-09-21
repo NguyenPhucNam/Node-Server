65e078721c948e3061f7025c1e8f725febcad7bd
NguyenPhucNam <nnam1472@gmail.com>
2019-09-21 15:04:36 +0700

    change port server

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const app = require("./app");
 const config = require('./config/config');
 const server = require("http").Server(app);
 
-const port = process.env.PORT || config.Port;
-server.listen(port, '0.0.0.0', () => {
+const port = config.Port || process.env.PORT;
+server.listen(port, '127.0.0.1', (err) => {
+    if (err) {
+        console.log(err.stack);
+        return;
+    }
+
     console.log("Start server: " + port);
 });
